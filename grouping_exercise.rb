2a26beef666f7c7b496df4fb6c8acb43ba6f2bdf
Timothy Andrew <mail@timothyandrew.net>
2013-10-23 15:00:24 +0530

    Initial commit.

---
+++ grouping_exercise.rb
@@ -0,0 +1,1 @@
+Dir[File.dirname(__FILE__) + "/src/*.rb"].each { |file| require file }
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'bundler/setup'
+require_relative '../grouping_exercise'
+
+RSpec.configure do |config|
+end
