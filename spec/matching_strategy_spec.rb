20ac6a30a1a3dd377ac146f3134c9110fc35bfdf
Timothy Andrew <mail@timothyandrew.net>
2013-10-24 12:38:24 +0530

    MatchingStrategy maintains a list of available matching types.
    - So far just `same_email`
    - The shell script passes the user-entered type into MatchingStrategy

--- spec/matching_strategy_spec.rb
+++ spec/matching_strategy_spec.rb
@@ -3,16 +3,16 @@ require 'spec_helper'
 describe MatchingStrategy do
   context "while matching by email" do
     it "matches two rows which have the same value for an attribute" do
-      strategy = MatchingStrategy.new(attribute: MatchingAttribute.new('email'))
-      first_row = Row.new('email' => "foo@foo.com")
-      second_row = Row.new('email' => "foo@foo.com")
+      strategy = MatchingStrategy.new_with_type('same_email')
+      first_row = Row.new('Email' => "foo@foo.com")
+      second_row = Row.new('Email' => "foo@foo.com")
       strategy.match?(first_row, second_row).should be_true
     end
 
     it "doesn't match two rows which have different values for the same attribute" do
-      strategy = MatchingStrategy.new(attribute: MatchingAttribute.new('email'))
-      first_row = Row.new('email' => "foo@foo.com")
-      second_row = Row.new('email' => "foo@bar.com")
+      strategy = MatchingStrategy.new_with_type('same_email')
+      first_row = Row.new('Email' => "foo@foo.com")
+      second_row = Row.new('Email' => "foo@bar.com")
       strategy.match?(first_row, second_row).should_not be_true
     end
   end
--- src/matching_strategy.rb
+++ src/matching_strategy.rb
@@ -3,11 +3,21 @@ class MatchingStrategy
     @matching_attribute = config[:attribute]
   end
 
+  def self.new_with_type(type)
+    strategies[type]
+  end
+
   def match?(first, second)
     first.get(@matching_attribute.key) == second.get(@matching_attribute.key)
   end
 
   def key_for(first, second)
     "#{first.get(@matching_attribute.key)}#{second.get(@matching_attribute.key)}"
+  end
+
+  private
+
+  def self.strategies
+    { 'same_email' => MatchingStrategy.new(attribute: MatchingAttribute.new('Email')) }
   end
 end
